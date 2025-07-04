a8e597aad0f17cb3a918c519c08969d00c801dfa
POLAK9145 <riwangabryszewski45@gmail.com>
2025-07-04 19:52:53 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>🇵🇱 Apprends le polonais</h1>
+      <p>Bienvenue sur ton site d'apprentissage du polonais !</p>
+    </div>
+  );
+}
